38ddd368c90aa82f0d05df56796d717443a3fcb9
AselaDK <aselax2@gmail.com>
2019-11-07 18:58:05 +0530

    create passport config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
+
+var authRouter = require('./auth');
+var paperRouter = require('./questions');
+var userRouter = require('./users');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
+const passportJWT = passport.authenticate('jwt', { session: false });
+
+
+router.use('/auth',authRouter);
+router.use('/questions',passportJWT,paperRouter);
+router.use('/users',passportJWT,userRouter);
 
 module.exports = router;
