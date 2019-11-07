02504e01126c7a64b008441d376a2f808f51a7d7
tharinduprabath <tharindurulz.mc@gmail.com>
2019-11-07 18:04:44 +0530

    google and local user model

--- models/user.js
+++ models/user.js
@@ -1,26 +1,52 @@
 const mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-    _id:{
-        type:String,
-        required:true
+    method: {
+        type: String,
+        //more methods will add
+        enum: ['google', 'local'],
+        required: true
     },
-    username:{
-        type:String,
-        required:true
+    local: {
+        email:{
+            type:String,
+            required:true
+        },
+        username:{
+            type:String,
+            required:true
+        },
+        passwd:{
+            type:String,
+            required:true
+        },
+        signuptime:{
+            type:Date,
+            required:true
+        },
+        photourl:{
+            type:String,
+            required:true,
+        },
     },
-    passwd:{
-        type:String,
-        required:true
-    },
-    signuptime:{
-        type:Date,
-        required:true
-    },
-    photourl:{
-        type:String,
-        required:true,
-    },
+    google: {
+        email:{
+            type:String,
+            required:true
+        },
+        username:{
+            type:String,
+            required:true
+        },
+        signuptime:{
+            type:Date,
+            required:true
+        },
+        photourl:{
+            type:String,
+            required:true,
+        },
+    }
 });
 
 module.exports = mongoose.model('user',userSchema);
